ed08d320fb4aca706529b65796311698729a0bf4
Martin Heidegger <martin.heidegger@gmail.com>
2019-02-19 12:23:16 +0900

    More tests, some fixes.

--- test/lib/EventedMapOfSets.js
+++ test/lib/EventedMapOfSets.js
@@ -3,51 +3,66 @@ const EventedMapOfSets = require('../../lib/EventedMapOfSets.js')
 const tape = require('tape')
 const toArray = require('../../lib/iter/toArray.js')
 
-tape('basics: empty', t => {
+function test (name, fn) {
+  tape(name, t => {
+    fn(t)
+      .catch(err => t.fail(err))
+      .then(() => t.end())
+  })
+}
+
+test('basics: empty', async t => {
   const set = new EventedMapOfSets()
   t.equals(set.hasEntry('x', 'y'), false)
   for (const entry of set.get('x')) {
     t.fail('Unexpected entry: ' + entry)
   }
-  t.end()
 })
 
-tape('basics: add', t => {
+test('basics: add', async t => {
   const set = new EventedMapOfSets()
-  set.add('x', 'y')
+  t.equals(await set.add('x', 'y'), true)
   t.equals(set.hasEntry('x', 'y'), true)
-  set.add('x', 'y') // repeat call
+  t.equals(await set.add('x', 'y'), false, 'repeat call')
   t.deepEquals(toArray(set.get('x')), ['y'])
-  set.add('x', 'z')
+  await set.add('x', 'z')
   t.deepEquals(toArray(set.get('x')), ['y', 'z'])
-  t.end()
 })
 
-tape('basics: remove', t => {
+test('basics: remove', async t => {
   const set = new EventedMapOfSets()
-  set.add('x', 'y')
-  set.delete('x', 'y')
+  t.equals(await set.delete('x', 'y'), false)
+  await set.add('x', 'y')
+  t.equals(await set.delete('x', 'z'), false)
+  t.equals(await set.delete('x', 'y'), true)
   t.equals(set.hasEntry('x', 'y'), false)
   for (const entry of set.get('x')) {
     t.fail('Unexpected entry: ' + entry)
   }
-  t.end()
 })
 
-tape('basics: add-hash', t => {
+test('basics: add-hash', async t => {
   const set = new EventedMapOfSets()
-  set.add('x', 'y', 'r')
-  set.add('x', 'z', 'r')
+  t.equals(await set.add('x', 'y', 'r'), true)
+  t.equals(await set.add('x', 'z', 'r'), false, 'different item with same hash')
   t.equals(set.hasEntry('x', 'r'), true)
   t.equals(set.hasEntry('x', 'y'), false)
   t.equals(set.hasEntry('x', 'z'), false)
   for (const entry of set.get('x')) {
     t.equals(entry, 'y')
   }
-  t.end()
 })
 
-tape('events', t => {
+test('basics: clear', async t => {
+  const set = new EventedMapOfSets()
+  await set.add('x', 'y')
+  await set.add('a', 'b')
+  await set.clear()
+  t.equals(set.hasEntry('x', 'y'), false)
+  t.equals(set.hasEntry('a', 'b'), false)
+})
+
+test('events', async t => {
   const set = new EventedMapOfSets()
   const stack = []
   set.on('add', (key, value, valueOrHash) => {
@@ -56,16 +71,24 @@ tape('events', t => {
   set.on('delete', (key, value, valueOrHash) => {
     stack.push({ del: { key, value, valueOrHash }})
   })
-  set.add('x', 'r', 'y')
-  set.add('x', 'y')
-  set.add('x', 'z')
-  set.delete('x', 'y')
-  set.delete('x', 'z')
+  await Promise.all([
+    set.add('x', 'r', 'y'),
+    set.add('x', 'y'),
+    set.add('x', 'z'),
+    set.delete('x', 'y'),
+    set.delete('x', 'z'),
+    set.add('a', 'b'),
+    set.add('x', 'y'),
+    set.clear()
+  ])
   t.deepEquals(stack, [
     { add: { key: 'x', value: 'r', valueOrHash: 'y' } },
     { add: { key: 'x', value: 'z', valueOrHash: 'z' } },
     { del: { key: 'x', value: 'r', valueOrHash: 'y' } },
-    { del: { key: 'x', value: 'z', valueOrHash: 'z' } }
+    { del: { key: 'x', value: 'z', valueOrHash: 'z' } },
+    { add: { key: 'a', value: 'b', valueOrHash: 'b' } },
+    { add: { key: 'x', value: 'y', valueOrHash: 'y' } },
+    { del: { key: 'a', value: 'b', valueOrHash: 'b' } },
+    { del: { key: 'x', value: 'y', valueOrHash: 'y' } }
   ])
-  t.end()
 })
---
+++ test/lib/serviceLookup.js
@@ -0,0 +1,72 @@
+'use strict'
+const tape = require('tape')
+const serviceLookup = require('../../lib/serviceLookup.js')
+
+tape('error: empty config', t => {
+  try {
+    serviceLookup({})(null)
+    t.fail('No Error thrown')
+  } catch (err) {
+    t.equals(err.code, 'ENOSERVICE')
+  }
+  t.end()
+})
+
+tape('error: multiple config', t => {
+  try {
+    serviceLookup({})({ a: {}, b: {} })
+    t.fail('No Error thrown')
+  } catch (err) {
+    t.equals(err.code, 'EMULTISERVICE')
+    t.deepEquals(err.keys, ['a', 'b'])
+  }
+  t.end()
+})
+
+tape('error: unknown config', t => {
+  try {
+    serviceLookup({})({ a: {} })
+    t.fail('No Error thrown')
+  } catch (err) {
+    t.equals(err.code, 'EUNKOWNSERVICE')
+    t.equals(err.type, 'a')
+  }
+  t.end()
+})
+
+tape('error: validation error', t => {
+  const err = new Error()
+  try {
+    serviceLookup({
+      a: {
+        validate () {
+          throw err
+        }
+      }
+    })({
+      a: {}
+    })
+    t.fail('No error thrown')
+  } catch (thrownErr) {
+    t.equal(thrownErr, err)
+  }
+  t.end()
+})
+
+tape('normal operation', t => {
+  const conf = {}
+  let called = 0
+  const factory = serviceLookup({
+    a: {
+      validate: () => {},
+      create (receivedConfig) {
+        called += 1
+        t.equals(conf, receivedConfig)
+      }
+    }
+  })({ a: conf })
+  t.equals(called, 0)
+  factory.create()
+  t.equals(called, 1)
+  t.end()
+})
