d952688bcbc2892a455dc95c5a3cd0757d85cd4c
Martin Heidegger <martin.heidegger@gmail.com>
2019-03-01 20:52:25 +0900

    Flipping operation order in reduce & map to fit filter.

--- test/lib/iter/map.js
+++ test/lib/iter/map.js
@@ -4,9 +4,9 @@ const toArray = require('../../../lib/iter/toArray.js')
 const tape = require('tape')
 
 tape('map', t => {
-  t.deepEquals(toArray(map([])), undefined, 'empty')
-  t.deepEquals(toArray(map([1], x => x)), [1], 'one item')
-  t.deepEquals(toArray(map([2], x => x + 1 )), [3], 'one item with op')
-  t.deepEquals(toArray(map([1, 2, 3][Symbol.iterator](), x => x * x)), [1, 4, 9], 'iterable with op')
+  t.deepEquals(toArray(map(undefined, [])), undefined, 'empty')
+  t.deepEquals(toArray(map(x => x, [1])), [1], 'one item')
+  t.deepEquals(toArray(map(x => x + 1, [2])), [3], 'one item with op')
+  t.deepEquals(toArray(map(x => x * x, [1, 2, 3][Symbol.iterator]())), [1, 4, 9], 'iterable with op')
   t.end()
 })
