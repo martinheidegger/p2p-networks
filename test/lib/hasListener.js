c8d5a1f84c3faaa94b8ccbd609f7d7d0ac26836a
Martin Heidegger <martin.heidegger@gmail.com>
2019-03-04 17:00:35 +0900

    Added test for hasListener.

---
+++ test/lib/hasListener.js
@@ -0,0 +1,23 @@
+'use strict'
+const tape = require('tape')
+const { EventEmitter } = require('events')
+const hasListener = require('../../lib/hasListener.js')
+
+tape('simple listener support', t => {
+  const emitter = new EventEmitter()
+  let active = null
+  const stop = hasListener(emitter, 'test', isActive => active = isActive)
+  t.equals(active, false)
+  emitter.addListener('other', () => {})
+  t.equals(active, false)
+  emitter.addListener('test', () => {})
+  t.equals(active, true)
+  emitter.removeAllListeners('test')
+  t.equals(active, false)
+  emitter.addListener('test', () => {})
+  t.equals(active, true)
+  stop()
+  emitter.removeAllListeners()
+  t.equals(active, true)
+  t.end()
+})
