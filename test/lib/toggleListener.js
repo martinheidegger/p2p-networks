14c4a8caa5b598e3f021d4ef871e81d4cac1c123
Martin Heidegger <martin.heidegger@gmail.com>
2019-03-04 17:20:56 +0900

    Added toggleListener util to make certain that listeners are added/removed from a target.

---
+++ test/lib/toggleListener.js
@@ -0,0 +1,16 @@
+'use strict'
+const tape = require('tape')
+const { EventEmitter } = require('events')
+const toggleListener = require('../../lib/toggleListener.js')
+
+tape('simple toggling of listener support', t => {
+  const emitter = new EventEmitter()
+  const handler = () => {}
+  toggleListener(emitter, 'test', handler, false)
+  t.equals(emitter.listenerCount('test'), 0)
+  toggleListener(emitter, 'test', handler, true)
+  t.equals(emitter.listenerCount('test'), 1)
+  toggleListener(emitter, 'test', handler, false)
+  t.equals(emitter.listenerCount('test'), 0)
+  t.end()
+})
