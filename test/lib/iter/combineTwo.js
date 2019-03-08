bff700b588bee015e20e5663059ed3c67694dcaa
Martin Heidegger <martin.heidegger@gmail.com>
2019-03-08 21:21:31 +0900

    Added specialized combine variant combineTwo

---
+++ test/lib/iter/combineTwo.js
@@ -0,0 +1,13 @@
+'use strict'
+const combineTwo = require('../../../lib/iter/combineTwo.js')
+const toArray = require('../../../lib/iter/toArray.js')
+const tape = require('tape')
+
+tape('combine with iterators', t => {
+  t.deepEquals(toArray(combineTwo()), undefined, 'empty')
+  t.deepEquals(toArray(combineTwo([])), undefined, 'empty item')
+  t.deepEquals(toArray(combineTwo([1], [2])), [1, 2], 'two arrays')
+  t.deepEquals(toArray(combineTwo([1, 2], [3, 4])), [1, 2, 3, 4], 'items with more than one entry')
+  t.deepEquals(toArray(combineTwo([1][Symbol.iterator]())), [1], 'With a simple iterator')
+  t.end()
+})
