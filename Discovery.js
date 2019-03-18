31c925d8bbfec229ed73f21f8687902783acdcd4
Martin Heidegger <martin.heidegger@gmail.com>
2019-03-18 23:00:21 +0900

    Updated discovery and discoverySet to reflect the implementation state of dht & dht-legacy.

--- Discovery.js
+++ Discovery.js
@@ -1,132 +1,115 @@
 'use strict'
 const EventEmitter = require('events').EventEmitter
 const serviceLookup = require('./lib/serviceLookup.js')
-const EventedMapOfSets = require('./lib/EventedMapOfSets.js')
 const rangeInterval = require('./lib/rangeInterval.js')
 const stateMgr = require('./lib/stateMgr.js')
+const hasListener = require('./lib/hasListener.js')
+const toggleListener = require('./lib/toggleListener.js')
 
+const State = Object.freeze({
+  announce: Symbol('announce'), // Publications available to announce, lets share this with the network
+  lookup: Symbol('lookup'), // No publication available, lets use lookup to see if we can find some peer anyways
+  inactive: Symbol('inactive'), // Nothing to look for, nothing to offer. Waiting for either
+  closed: Symbol('closed'), // Already closed
+  waiting: Symbol('waiting') // No connection available: Waiting for the setup to be ready
+})
 
-const LOOKUP = {
-  IDLE: Symbol('Idle.'),
-  ACTIVE: Symbol('Sending a lookup event following an interval'),
-  INACTIVE: Symbol('Waiting for keys to exist before activation')
-}
+const ACTIVE_STATES = [State.announce, State.lookup, State.inactive]
 
 class Discovery extends EventEmitter {
-  static STATE_ANNOUNCE = Symbol('announce - Publications available to announce, lets share this with the network')
-  static STATE_LOOKUP = Symbol('lookup - No publication available, lets use lookup to see if we can find some peer anyways.')
-  static STATE_CLOSED = Symbol('closed')
-  static STATE_WAITING = Symbol('waiting - No connection available')
+  static State = State
 
-  constructor (services, config, keyByAddress) {
-    super()
-    this._config = config
-    this._peers = new EventedMapOfSets()
-    this.closed = new Promise(resolve => this.on('close', resolve))
-
-    const service = serviceLookup(services, config).create(this, this._peers, keyByAddress)
-    const removeKeyAdress = (key, address) => {
-      service.unannounce(key, address)
-      if (keyByAddress.y.size === 0) {
-        state.set(Discovery.STATE_LOOKUP)
-      }
-    }
-    const setStateToAnnounce = () => {
-      state.set(Discovery.STATE_ANNOUNCE)
-    }
-    const unlistening = () => {
-      state.set(Discovery.STATE_WAITING)
-      setTimeout(() => service.open(), config.reconnect)
-    }
-    const activateLookup = () => {
-      lookupState.set(LOOKUP.ACTIVE)
-    }
-    const testDeactivateLookup = () => {
-      if (keyByAddress.x.size > 0) return
-      lookupState.set(LOOKUP.INACTIVE)
-    }
-    const lookupRange = rangeInterval.parse(config.lookupInterval || '10~15min')
-    let lookupInterval
-    const lookupState = stateMgr((newState, oldState) => {
-      switch (oldState) {
-        case LOOKUP.ACTIVE:
-          rangeInterval.clear(lookupInterval)
-          keyByAddress.x.removeListener('add', service.lookup)
-          keyByAddress.x.removeListener('remove', testDeactivateLookup)
-          break
-        case LOOKUP.INACTIVE:
-          keyByAddress.x.removeListener('add', activateLookup)
-          break
-      }
-      switch (newState) {
-        case LOOKUP.ACTIVE:
-          keyByAddress.x.on('remove', testDeactivateLookup)
-          keyByAddress.x.on('add', service.lookup)
-          for (const key of keyByAddress.x) {
-            service.lookup(key)
-          }
-          lookupInterval = rangeInterval.set(() => {
-            for (const key of keyByAddress) {
-              service.lookup(key)
-            }
-          }, lookupRange)
-          break
-        case LOOKUP.INACTIVE:
-          keyByAddress.x.on('add', activateLookup)
-          break
-      }
-    }, LOOKUP.IDLE)
-    const listening = () => state.set(keyByAddress.y.size === 0 ? Discovery.STATE_LOOKUP : Discovery.STATE_ANNOUNCE)
-    const state = stateMgr((newState, oldState) => {
-      switch (oldState) {
-        case Discovery.STATE_ANNOUNCE:
-          keyByAddress.removeListener('add', service.announce)
-          keyByAddress.removeListener('remove', removeKeyAdress)
-          break
-        case Discovery.STATE_LOOKUP:
-          lookupState.set(LOOKUP.IDLE)
-          keyByAddress.y.removeKeyAdress('add', setStateToAnnounce)
-          break
-      }
-      switch (newState) {
-        case Discovery.STATE_ANNOUNCE:
-          keyByAddress.addListener('add', service.announce)
-          keyByAddress.addListener('remove', removeKeyAdress)
-          for (const tuple of keyByAddress) {
-            service.announce(tuple[0], tuple[1])
-          }
-          break
-        case Discovery.STATE_LOOKUP:
-          lookupState.set(keyByAddress.x.size > 0 ? LOOKUP.ACTIVE : LOOKUP.INACTIVE)
-          keyByAddress.y.addListener('add', setStateToAnnounce)
-          break
-        case Discovery.STATE_CLOSED:
-          service.close()
-          this.removeListener('listening', listening)
-          this.removeListener('unlistening', unlistening)
-          break
-      }
-    }, Discovery.STATE_WAITING)
-
-    this._state = state
-    this.on('listening', listening)
-    this.on('unlistening', unlistening)
-    service.open()
+  validate (services, config) {
+    serviceLookup(services, config) // validation happens here!
   }
 
-  get peers () {
-    return this._peers
+  constructor (services, config, peers, keyByAddress) {
+    super()
+    this._config = config
+    const { lookupInterval: configLookupInterval, ... serviceConfig } = config
+    const lookupRange = rangeInterval.parse(configLookupInterval)
+    this.closed = new Promise(resolve => this.once('close', resolve))
+    const service = serviceLookup(services, serviceConfig).create(this, peers)
+    let logActive = false
+    const closeLog = hasListener(this, 'log', isActive => logActive = isActive)
+    const unlistening = () => {
+      if (logActive) this.emit('log', { type: 'unlistening' })
+      state.set(State.waiting)
+      setTimeout(() => service.open(), config.reconnect)
+    }
+    const checkState = () => {
+      if (keyByAddress.keys.size === 0) {
+        return state.set(State.inactive)
+      }
+      if (keyByAddress.values.size === 0) {
+        return state.set(State.lookup)
+      }
+      state.set(State.announce)
+    }
+    const lookup = () => {
+      if (logActive) this.emit('log', { type: 'lookup' })
+      for (const key of keyByAddress.keys) {
+        service.lookup(key)
+      }
+    }
+    const isActive = stateMgr(isActive => {
+      toggleListener(keyByAddress, 'add', checkState, isActive)
+      toggleListener(keyByAddress, 'remove', checkState, isActive)
+    }, false)
+    const isAnnouncing = stateMgr(isAnnouncing => {
+      toggleListener(keyByAddress, 'add', service.announce, isAnnouncing)
+      toggleListener(keyByAddress, 'remove', service.unannounce, isAnnouncing)
+      if (isAnnouncing) {
+        for (const [key, address] of keyByAddress) {
+          service.announce(key, address)
+        }
+      }
+    }, false)
+    let lookupInterval
+    const isLookup = stateMgr(isLookup => {
+      if (isLookup) {
+        lookupInterval = rangeInterval.set(lookup, lookupRange)
+        lookup()
+      } else {
+        rangeInterval.clear(lookupInterval)
+      }
+    }, false)
+    const state = stateMgr(state => {
+      if (logActive) this.emit('log', { type: 'state-change', state })
+      isLookup.set(state === State.lookup)
+      isAnnouncing.set(state === State.announce)
+      isActive.set(ACTIVE_STATES.includes(state))
+      if (state === State.closed) {
+        this.removeListener('listening', checkState)
+        this.removeListener('unlistening', unlistening)
+        service.close()
+        this.once('close', () => {
+          if (logActive) this.emit('log', { type: 'closed' })
+          closeLog()
+        })
+      }
+    }, State.waiting)
+
+    this._state = state
+    this.on('listening', checkState)
+    this.on('unlistening', unlistening)
+    service.open()
   }
 
   get config () {
     return this._config
   }
 
+  get state () {
+    return this._state.get()
+  }
+
   close (cb) {
-    this._state.set(Discovery.STATE_CLOSED)
+    this._state.set(State.closed)
     if (cb) {
       return this.closed.then(cb)
     }
+    return this.closed
   }
 }
 
--- DiscoverySet.js
+++ DiscoverySet.js
@@ -1,21 +1,19 @@
 'use strict'
 const Discovery = require('./Discovery.js')
 const ConfigSet = require('./ConfigSet.js')
-const EventedMergedMap = require('./lib/EventedMergedMap.js')
 
 class DiscoverySet extends ConfigSet {
 
-  constructor (services, keyByAddress) {
+  constructor (services, peers, keyByAddress) {
     super((config, cb) => {
       let discovery
       try {
-        discovery = new Discovery(services, config, keyByAddress)
+        discovery = new Discovery(services, config, peers, keyByAddress)
       } catch (err) {
         return cb(err)
       }
       cb(null, {
         close: cb => {
-          this._peers.delete(discovery.peers)
           discovery.close((err) => {
             if (err) return cb(err)
             discovery.removeAllListeners()
@@ -24,12 +22,6 @@ class DiscoverySet extends ConfigSet {
         }
       })
     })
-
-    this._peers = new EventedMergedMap()
-  }
-
-  get peers () {
-    return this._peers
   }
 }
 
