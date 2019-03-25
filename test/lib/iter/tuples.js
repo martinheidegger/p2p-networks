228c9a3c3619e0493a94d638929c531731fbf01d
Martin Heidegger <martin.heidegger@gmail.com>
2019-03-25 10:44:51 +0900

    Making sure that the first parameter is passed to the tuples test.

--- test/lib/iter/tuples.js
+++ test/lib/iter/tuples.js
@@ -1,6 +1,7 @@
 'use strict'
 const tuples = require('../../../lib/iter/tuples.js')
 const toArray = require('../../../lib/iter/toArray.js')
+const iterate = require('../../../lib/iter/iterate.js')
 const tape = require('tape-x')()
 
 tape('basics', t => {
@@ -11,4 +12,21 @@ tape('basics', t => {
   t.deepEquals(toArray(tuples([1, 'a', 'r', 2, 'b'], x => typeof x === 'number', y => true, (x, y) => y + x)), ['a1', 'b2'], 'non-matching interloper')
   t.deepEquals(toArray(tuples([1, 'a', 2, 'b'], () => true, () => true)), [[1, 'a'], [2, 'b']], 'Without each operator')
   t.end()
+})
+
+tape('second check receives first entry', t => {
+  iterate(
+    tuples([1, 'a'],
+      x => {
+        t.equals(x, 1)
+        return true
+      },
+      (y, x) => {
+        t.equals(x, 1)
+        t.equals(y, 'a')
+        return true
+      },
+      _ => t.end()
+    )
+  )
 })
